24fa1672862bd2b51578b61f2e7ac34cd1b71ad3
LitStronger <1589015065@qq.com>
2020-09-02 16:22:27 +0800

    整合驱动彩印机的代码

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,ipcMain } = require('electron')
 const electron = require('electron')
+const path = require('path')
+var ipp = require('ipp')
+var fs = require('fs')
 
 const Menu = electron.Menu
 
 function createWindow () {   
   // 隐藏菜单栏
-  Menu.setApplicationMenu(null)
+  // Menu.setApplicationMenu(null)
   // 创建浏览器窗口
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
     },
-    fullscreen: true,
+    // fullscreen: true,
+  })
+  ipcMain.on("print", function (e, data) {
+    console.log(data)
+    console.log("printing now!")
+
+    // var printer = ipp.Printer("http://127.0.0.1:631/printers/EPSON_L805_Series")
+
+    // var msg = {
+    //         "operation-attributes-tag": {
+    //             "requesting-user-name": "William",
+    //             "job-name": "My Test Job",
+    //             "document-format": "application/octet-stream",
+    //     },
+    //     data: data
+    // }; 
+
+    // printer.execute("Print-Job", msg, (err,res) =>{
+    //     console.log(res);
+    // })
   })
 
   // 并且为你的应用加载index.html
--- public/js/timer.js
+++ public/js/timer.js
@@ -2,7 +2,7 @@ var currentUrl = window.location.href;            //获取当前页面地址
 var flag =  currentUrl.split('/').pop()
 var timefly = 5000;         //超时时间
 var beginDt = new Date();        //把得到的当前时间放入变量作为初始时间
-var gourl = "/pages/ad.html";        //需要跳转的地址
+var gourl = "pages/ad.html";        //需要跳转的地址
     // document.onmousemove = retime;        //当鼠标移动时的时间
     // document.onkeypress = retime;        //当鼠标点击重新获取当前时间放入变量作为初始时间
     document.onclick = retime
@@ -18,6 +18,8 @@ var gourl = "/pages/ad.html";        //需要跳转的地址
         nowDt = new Date();        //得到当前时间放入变量
         console.log(nowDt - beginDt)
         if((nowDt - beginDt) > timefly){
-            location.href = gourl;     //如果当前时间减去初始时间大于超时时间，就执行自动跳转
+            console.log(gourl)
+            window.location = gourl
+            //location.href = gourl;     //如果当前时间减去初始时间大于超时时间，就执行自动跳转
         }
     }
