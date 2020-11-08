086ba16468db6512bef650b3632b3d49561a41dc
LitStronger <1589015065@qq.com>
2020-11-08 12:56:36 +0800

    爆改布局，之前写的太辣鸡了

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow,ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const electron = require('electron')
 const path = require('path')
 var ipp = require('ipp')
@@ -6,29 +6,30 @@ var fs = require('fs')
 
 const Menu = electron.Menu
 
-function createWindow () {   
-  // 隐藏菜单栏
-  // Menu.setApplicationMenu(null)
-  // 创建浏览器窗口
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 900,
-    webPreferences: {
-      nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js')
-    },
-    // fullscreen: true,
-  })
-  ipcMain.on("print", function (e, data) {
-    console.log(data)
-    console.log("printing now!")
-  })
+function createWindow() {
+    // 隐藏菜单栏
+    // Menu.setApplicationMenu(null)
+    // 创建浏览器窗口
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        },
+        // fullscreen: true,
+    })
 
-  // 并且为你的应用加载index.html
-  win.loadFile('index.html')
+    // ipcMain.on("print", function(e, data) {
+    //     console.log(data)
+    //     console.log("printing now!")
+    // })
 
-  // 打开开发者工具
-  win.webContents.openDevTools()
+    // 并且为你的应用加载index.html
+    win.loadFile('index.html')
+
+    // 打开开发者工具
+    win.webContents.openDevTools()
 }
 
 // Electron会在初始化完成并且准备好创建浏览器窗口时调用这个方法
@@ -37,19 +38,19 @@ app.whenReady().then(createWindow)
 
 //当所有窗口都被关闭后退出
 app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
 
 // 您可以把应用程序其他的流程写在在此文件中
